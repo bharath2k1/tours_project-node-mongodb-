b65a236a35e8a097910e96f4e727ec1687d7d391
BHARATH SIMHA REDDY <bharathreddy8345@gmail.com>
2020-09-28 15:01:57 +0530

    connected to mongodb atlas

--- server.js
+++ server.js
@@ -1,9 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+dotenv.config({ path: './config.env' });
 
 // console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('connected to db');
+  });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'name of the tour is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'the forest hiker',
+  rating: 4.6,
+  price: 499,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('error is', err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
